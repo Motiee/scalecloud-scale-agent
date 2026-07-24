9091a60e21f9f5c0ea3d1ba97fd8d488951820c0
Hesam Motiee <motiee@embedsys.ir>
2026-07-24 17:06:05 +0330

    TB Implemented

---
+++ Protocols/AlphaProtocol.cs
@@ -0,0 +1,48 @@
+﻿using scalecloud_scale_agent.Model;
+using scalecloud_scale_agent.Protocols.FrameDetectors;
+using scalecloud_scale_agent.Protocols.Interfaces;
+using System;
+using System.Text;
+
+public class AlphaProtocol : ScaleProtocolBase
+{
+    public override string Id => "Alpha";
+
+
+    public override string DisplayName => "Alpha";
+
+    public override decimal? PrevWeight { get; set; }
+
+    public AlphaProtocol() : base(new CarriageReturnFrameDetector()) { }
+    protected override bool ParseFrame(byte[] frame, out ScaleData data)
+    {
+        data = null;
+
+        if (frame.Length < 7) return false;
+
+        var text_Enc = Encoding.ASCII.GetString(frame).Trim();
+
+
+        var text = text_Enc.Trim().Substring(0, text_Enc.Length );
+
+   
+
+        if (!decimal.TryParse(
+            text,
+            out decimal weight))
+        {
+            return false;
+        }
+
+        data = new ScaleData
+        {
+            Weight = weight,
+            Stable = (weight == PrevWeight),
+            Time = DateTime.Now
+        };
+
+        PrevWeight = data.Weight;
+        return true;
+    }
+
+}
---
+++ Protocols/TBProtocol.cs
@@ -0,0 +1,49 @@
+﻿using scalecloud_scale_agent.Model;
+using scalecloud_scale_agent.Protocols.FrameDetectors;
+using scalecloud_scale_agent.Protocols.Interfaces;
+using System;
+using System.Text;
+
+public class TBProtocol : ScaleProtocolBase
+{
+    public override string Id => "TB";
+
+
+    public override string DisplayName => "TB";
+
+    public override decimal? PrevWeight { get; set; }
+
+    public TBProtocol() : base(new CommaFrameDetector()) { }
+    protected override bool ParseFrame(byte[] frame, out ScaleData data)
+    {
+        data = null;
+
+
+
+        var text_Enc = Encoding.ASCII.GetString(frame).Trim();
+
+
+        var text = text_Enc.Trim().Substring(0, 5);
+
+
+
+        if (!decimal.TryParse(
+            text,
+            out decimal weight))
+        {
+            return false;
+        }
+
+        data = new ScaleData
+        {
+            Weight = weight,
+            Stable = (weight == PrevWeight),
+            Time = DateTime.Now
+        };
+
+        PrevWeight = data.Weight;
+
+        return true;
+    }
+
+}
